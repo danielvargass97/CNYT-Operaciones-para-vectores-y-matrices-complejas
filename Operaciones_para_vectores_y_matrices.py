c4057185ed849088b2423a6cef240090a339b2f0
unknown <daniel.vargas-o@labinfo.is.escuelaing.edu.co>
2020-02-08 12:41:36 -0500

    Avance 1

--- Operaciones_para_vectores_y_matrices.py
+++ Operaciones_para_vectores_y_matrices.py
@@ -58,9 +58,35 @@ def adicionMatrices(A,B):
             matriz_respuesta.append(aux_fila)
 
         return matriz_respuesta
-                
+
+def multiplicacionEscalarMatriz(c, A):
+    '''La multiplicacion de un escalar por un vector se realiza multiplicando
+    cada punto de la matriz por el escalar. El resultado es una matriz'''
+
+    matriz_respuesta = []
+
+    for i in range(0, len(A)):
+            aux_fila = []
+            for j in range(0, len(A[0])):
+                aux_fila.append(NC.producto(c, A[i][j]))
+            matriz_respuesta.append(aux_fila)
+
+    return matriz_respuesta
+
+def inversoAditivoMatriz(A):
+    '''El inverso aditivo de una matriz compleja es el resultado de multiplicar
+    -1 a toda la matriz. El resultado es una matriz'''
+
+    return multiplicacionEscalarMatriz([-1,0],A)
+    
+    
                 
 
 #print(adicionVectores([ [8,3],[-1,-4],[0,-9] ],[ [8,-3],[2,5],[3,0] ]))
 #print(multiplicacionEscalarVector([-1,1], [ [-2,5],[-1,-1],[2,-9] ]))            
 #print(inversoAditivoVector([ [-5,2],[3,0],[0,-1] ]))
+#print(adicionMatrices([[ [-8,-3],[-6,-4],[0,-4] ],[ [-1,8],[6,-10],[8,-5] ],[ [4,0],[8,5],[-7,-9]]],
+#                      [[ [-7,-2],[-4,-2],[7,7] ],[ [5,9],[0,3],[6,-5] ],[ [1,5],[-6,-6],[5,8] ]]))
+#print(multiplicacionEscalarMatriz([-2,3],
+#                                  [[ [3,-2],[8,-4] ],[ [4,-10],[-2,-8]]]))
+print(inversoAditivoMatriz([ [ [7,3],[-1,7] ],[ [-9,-4],[-7,-9] ]]))
